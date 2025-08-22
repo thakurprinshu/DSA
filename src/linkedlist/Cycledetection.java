9e1a28d848d78a9a947e148d8ffc50398ee27acf
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-22 12:19:34 +0530

    addded the Cycle detection code

---
+++ src/linkedlist/Cycledetection.java
@@ -0,0 +1,40 @@
+package linkedlist;
+
+class Node {
+    int data;
+    Node next;
+    Node(int val) {
+        this.data = val;
+        this.next = null;
+    }
+}
+
+public class Cycledetection {
+
+    public static boolean hasCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(2);
+        head.next.next.next.next.next = head.next;
+
+        System.out.println(hasCycle(head));
+    }
+}

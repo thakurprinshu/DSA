f96e995d007887bc05069fe116c6ed9ca4ef103c
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-25 10:35:53 +0530

    added the code to find next greatest element

---
+++ src/stack/NextGreaterElement.java
@@ -0,0 +1,25 @@
+package stack;
+
+import java.util.*;
+
+public class NextGreaterElement {
+    public static int[] findnge(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 25};
+        int[] ans = findnge(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+}

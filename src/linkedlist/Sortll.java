1d660e3406f37852748a8088ecd43d9247ff5ea9
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-25 11:56:17 +0530

    added the code to merge linkedlist

--- src/linkedlist/Sortll.java
+++ src/linkedlist/Sortll.java
@@ -1,4 +1,64 @@
 package linkedlist;
 
 public class Sortll {
+    public static Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node middle = getMiddle(head);
+        Node nextOfMiddle = middle.next;
+        middle.next = null;
+        Node left = mergeSort(head);
+        Node right = mergeSort(nextOfMiddle);
+        return merge(left, right);
+    }
+    public static Node merge(Node l1, Node l2) {
+        Node dummy = new Node(-1);
+        Node curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.data <= l2.data) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) curr.next = l1;
+        if (l2 != null) curr.next = l2;
+
+        return dummy.next;
+    }
+    public static Node getMiddle(Node head) {
+        if (head == null) return head;
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    public static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.next = new Node(2);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(3);
+        System.out.println("Original List:");
+        printList(head);
+        head = mergeSort(head);
+        System.out.println("Sorted List:");
+        printList(head);
+    }
 }
+

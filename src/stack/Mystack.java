0d15234c2b0d310b3d7932ec6a04751aa596011d
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-26 10:44:07 +0530

    added my custom made stack class (Mystack)

---
+++ src/stack/Mystack.java
@@ -0,0 +1,57 @@
+package stack;
+
+public class Mystack {
+    private int arr[];
+    private int top;
+    private int capacity;
+
+    public Mystack(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
+
+    public void push(int x) {
+        if (isFull()) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        arr[++top] = x;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public void printStack() {
+        for (int i = 0; i <= top; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+

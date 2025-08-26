b2989ec4135bc23fb2f0d8182d79e6bf7251b35a
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-26 10:34:47 +0530

    added the code to search in rotated array

---
+++ src/SrchSort/Srchrotatedarray.java
@@ -0,0 +1,31 @@
+package SrchSort;
+
+public class Srchrotatedarray {
+    public static int search(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) return mid;
+            if (arr[left] <= arr[mid]) {
+                if (target >= arr[left] && target < arr[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else {
+                if (target > arr[mid] && target <= arr[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println("Index of " + target + " = " + search(arr, target));
+    }
+}
+
